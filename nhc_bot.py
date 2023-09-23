932948e0b553a1ceed468ce917740f5fde511226
Marcos Huerta <marcos@marcoshuerta.com>
2023-09-23 10:11:27 -0400

    better fstring

--- nhc_bot.py
+++ nhc_bot.py
@@ -33,46 +33,48 @@ def process_data(data_list):
     return out
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--nopost', action='store_true', default=False)
-
-    args = parser.parse_args()
-
-    test_data = process_data(process_url(CURRENT_URL))
+def make_post_content(data_for_post):
     clean_title = re.sub(r"\(.+\)", '',
-                         test_data['summary_title']).strip().replace(
+                         data_for_post['summary_title']).strip().replace(
                              "Tropical Storm", 'T.S.')
     pattern = r'\.\.\.(.*?)\.\.\.'
 
     # Use re.sub() to remove the ellipsis and replace with the captured text and a single period
-    cleaner_summary = re.sub(pattern, r'\1.', test_data['summary'])
+    cleaner_summary = re.sub(pattern, r'\1.', data_for_post['summary'])
 
     sentences = cleaner_summary.split(". ")
 
     non_headline = ". ".join(sentences[2:])
 
     rem = re.match(r"Tropical Storm (\S+) Public Advisory Number (.+)",
-                   test_data['full_advisory_title'])
+                   data_for_post['full_advisory_title'])
     advisory_number = rem.group(2)
 
-    post_content = f"""
-{clean_title}
+    ### F String
+    post_content = (
+        f"{clean_title}\n\n"
+        f"{sentences[0].strip()}.\n\n"
+        f"{sentences[1].strip()}.\n\n"
+        f"{non_headline}\n\n"
+        f"Graphics: {data_for_post['graphic_link']}\n"
+        f"Advisory {advisory_number}: {data_for_post['full_advisory_link']}\n\n"
+        f"#{rem.group(1)}")
 
-{sentences[0].strip()}
-{sentences[1].strip()}
+    return post_content
 
-{non_headline}
 
-{test_data['graphic_link']}
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nopost', action='store_true', default=False)
 
-Advisory {advisory_number}: {test_data['full_advisory_link']}
+    args = parser.parse_args()
 
-#{rem.group(1)}"""
+    data_for_post = process_data(process_url(CURRENT_URL))
+    post_content = make_post_content(data_for_post)
 
     if not args.nopost:
         m = Mastodon(access_token=API_TOKEN, api_base_url='https://vmst.io')
-        med_dict = m.media_post(test_data['graphic_data'],
+        med_dict = m.media_post(data_for_post['graphic_data'],
                                 mime_type='image/png')
         out = m.status_post(post_content, media_ids=med_dict)
         print(
@@ -80,3 +82,4 @@ Advisory {advisory_number}: {test_data['full_advisory_link']}
         )
     else:
         print(post_content)
+        print(f"Length: {len(post_content)}")
